b54f6bbb1f2bbd68db5f73c467617ded8e6ff010
Destinee <Destineerobinson08@gmail.com>
2021-07-30 21:56:22 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -4,34 +4,27 @@ const cats = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendCat(name){
     cats.push('Ralph');
 }
-
 function destructivelyPrependCat(name) {
     cats.unshift('Bob');
 }
- 
 function destructivelyRemoveLastCat() {
     cats.pop();
 }
-
 function destructivelyRemoveFirstCat() {
     cats.shift();
 }
-
 function appendCat(name) {
     const newCats = [...cats, name]
     return newCats;
 }
-
 function prependCat(name){
     const newCats = [name, ...cats]
     return newCats;
 }
-
 function removeLastCat(){
     const newCats = cats.slice(0, cats.length - 1);
     return newCats;
 }
-
 function removeFirstCat(){
     const newCats = cats.slice(1);
     return newCats;
